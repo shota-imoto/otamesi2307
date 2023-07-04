b8dc9d1f86a8afbdb4b7f44168ea144892c5afbc
shota-imoto <hideyoshi.playing.the.banjo@gmail.com>
2023-07-05 08:58:31 +0900

    シグナルを受け付けてグレースフルシャットダウンするように変更

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/shota-imoto/otamesi2307/config"
 	"golang.org/x/sync/errgroup"
@@ -20,6 +22,8 @@ func main() {
 }
 
 func run(ctx context.Context) error {
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
